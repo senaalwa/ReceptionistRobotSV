38a7a31895fcc14868236a0579d78efae29fc4c7
yunusanr <ynsclassdev@gmail.com>
2024-01-25 11:39:53 +0700

    done documented -new btn feature

--- Radio.ino
+++ Radio.ino
@@ -4,24 +4,30 @@
  */
 void initializeRadio() {
   radio.begin();
+  // Set alamat modul transceiver
   radio.openWritingPipe(address);
+  // Set tingkat transmisi, semakin tinggi semakin jauh dapat mentransmisi tp boros daya
   radio.setPALevel(RF24_PA_HIGH);
+  // Modul berfungsi sebagai pengirim
   radio.stopListening();
 }
 
 /**
- * @brief Mengambil data dari input joystick dan button lalu mengirimkan data melalui NRF
+ * @brief Mengambil data dari input joystick dan button lalu mengirimkan data melalui NRF24L01
  * 
  */
 void getSendData() {
+  // membaca input
   x_value = analogRead(X_AXIS);
   y_value = analogRead(Y_AXIS);
   btn_state = digitalRead(BUTTON);
 
+  // packing data
   payload[0] = x_value;
   payload[1] = y_value;
   payload[2] = btn_state;
 
+  // pengiriman data
   radio.write(&payload, sizeof(payload));
 }
 
--- RemoteTx.ino
+++ RemoteTx.ino
@@ -1,15 +1,19 @@
+// Include library
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// DESTRUCT OBJECT NRF DAN LCD I2C
-RF24 radio(7, 8);
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+// Inisialisasi object NRF24L01 dan LCD
+RF24 radio(7, 8); // CE=7, CSN=8
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Addr, Col, Row
 
 void setup() {
+  // Buka komunikasi serial dengan baud rate 9600
   Serial.begin(9600);
+
+  // Inisialisai perangkat radio nrf dan lcd i2c
   initializeMode();
   initializeRadio();
   initializeScreen();
@@ -19,8 +23,11 @@ void setup() {
 }
 
 void loop() {
+  // Ambil data dari joystick, button dan mengirim melalui NRF24L01
   getSendData();
+  // Menampilkan data pada LCD
   displayData();
+  // Menampilkan data pada serial monitor
   debugData();
 
   delay(100);
